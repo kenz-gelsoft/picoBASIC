b738be22ad443c7331a4cc77ef9113c4176de311
KENZ <KENZ.gelsoft@gmail.com>
2018-02-09 12:35:34 +0900

    Remove source textarea and add welcome message.

--- basic.js
+++ basic.js
@@ -4,19 +4,11 @@ window.addEventListener('load', function () {
 
 var console = null;
 function main() {
-    var button = document.getElementById('run');
-    button.addEventListener('click', function () {
-//        testTokenizer();
-        var lines = document.getElementById('source').value.split('\n');
-        lines.forEach(function (aLine) {
-            parseLine(aLine);
-        });
-    }, false);
-    
     console = new Console('console');
     
-    LOCATE(10, 5);
-    PUT('This is console.');
+    PUT('WeBASIC version 0.0');
+    PUT('COPYRIGHT (C) WeBASIC PROJECT.');
+    PUT('Ok ');
 }
 function parseLine(aLine, aPrintOk) {
     var ss = new StringStream(aLine);
