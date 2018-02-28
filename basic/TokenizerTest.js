5f563be0e7f7fd44dc5393965edfa53074dc9f0c
KENZ <KENZ.gelsoft@gmail.com>
2018-03-01 07:16:30 +0900

    Split tokenizer test into multiple tests

--- basic/TokenizerTest.js
+++ basic/TokenizerTest.js
@@ -1,10 +1,27 @@
 describe('Tokenizer', function () {
-    it('should parse valid tokens', function() {
-        testTokenizer();
-    });
-});
-function testTokenizer() {
-    const tests = [
+    const tokenize = function (aLine) {
+        const ss = new StringStream(aLine);
+        const tr = new Tokenizer(ss);
+        const tokens = [];
+        while (true) {
+            const t = tr.next();
+            if (t == null) {
+                break;
+            }
+            tokens.push(t);
+        }
+        return tokens;
+    };
+    const testLines = function () {
+        for (const test of arguments) {
+            const line   = test.shift();
+            const answer = test.shift();
+            expect(tokenize(line)).to.eql(answer);        
+        }
+    };
+   
+    it('should support strings', function () {
+        testLines(
         ['PRINT"HELLO \\\"WORLD\\\""', [
             new Token(Token.IDENT, 'PRINT'),
             new Token(Token.STRING, '"HELLO \\"WORLD\\""'),
@@ -15,17 +32,29 @@ function testTokenizer() {
             new Token(Token.STRING, '"HELLO"'),
             new Token(Token.EOS, ''),
         ]],
+        );
+    });
+    it('should support integers', function () {
+        testLines(
         ['123,', [
             new Token(Token.INT, '123'),
             new Token(Token.COMMA, ','),
             new Token(Token.EOS, ''),
         ]],
+        );
+    });
+    it('should support variable assignments', function () {
+        testLines(
         ['I=3029.9', [
             new Token(Token.IDENT, 'I'),
             new Token(Token.EQUAL, '='),
             new Token(Token.FLOAT, '3029.9'),
             // FIXME: here should be EOS
         ]],
+        );
+    });
+    it('should support expressions', function () {
+        testLines(
         ['I=1+3', [
             new Token(Token.IDENT, 'I'),
             new Token(Token.EQUAL, '='),
@@ -56,6 +85,10 @@ function testTokenizer() {
             new Token(Token.MUL, '*'),
             new Token(Token.INT, '2'),
         ]],
+        );
+    });
+    it('should support other statements', function() {
+        testLines(
         ['SCREEN 1, 2', [
             new Token(Token.IDENT, 'SCREEN'),
             new Token(Token.INT, '1'),
@@ -78,20 +111,6 @@ function testTokenizer() {
             new Token(Token.COMMA, ','),
             new Token(Token.INT, '3'),
         ]],
-    ];
-    for (pair of tests) {
-        const line = pair.shift();
-        const answer = pair.shift();
-        const ss = new StringStream(line);
-        const tr = new Tokenizer(ss);
-        const tokens = [];
-        while (true) {
-            const t = tr.next();
-            if (t == null) {
-                break;
-            }
-            tokens.push(t);
-        }
-        expect(tokens).to.eql(answer);
-    }
-}
+        );
+    });
+});
