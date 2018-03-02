e38e0018c95afc963cb15a71fb0161a0c69c6e73
KENZ <kenz-gelsoft@users.noreply.github.com>
2018-03-02 23:58:58 +0900

    Remove id from the dummy textarea
    it's not necessary anymore

--- runtime/SIPController.js
+++ runtime/SIPController.js
@@ -1,21 +1,18 @@
 class SIPController {
-    constructor(aId='dummy') {
-        this.id = aId;
-        this.element = this.ensureDummyTextArea();
+    constructor() {
+        this.ensureDummyTextArea();
     }
     ensureDummyTextArea() {
-        var dummy = document.getElementById(this.id);
-        if (dummy) {
-            return dummy;
+        if (this.element) {
+            return;
         }
-        dummy = document.createElement('textarea');
-        dummy.id = this.id;
+        const dummy = document.createElement('textarea');
         dummy.style.position = 'absolute';
         dummy.style.left = '-10em';
         dummy.style.top  = '-10em';
         dummy.style.fontSize = '16px';
         document.body.appendChild(dummy);
-        return dummy;
+        this.element = dummy;
     }
     show() {
         this.element.focus();
