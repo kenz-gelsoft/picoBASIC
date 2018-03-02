9142af2dc61629dc1cac77d83d6fc1cce594794a
KENZ <kenz-gelsoft@users.noreply.github.com>
2018-03-03 01:12:00 +0900

    Simplify SIPController

--- runtime/SIPController.js
+++ runtime/SIPController.js
@@ -1,18 +1,12 @@
 class SIPController {
     constructor() {
-        this.ensureDummyTextArea();
-    }
-    ensureDummyTextArea() {
-        if (this.element) {
-            return;
-        }
-        const dummy = document.createElement('textarea');
-        dummy.style.position = 'absolute';
-        dummy.style.left = '-10em';
-        dummy.style.top  = '-10em';
-        dummy.style.fontSize = '16px';
-        document.body.appendChild(dummy);
-        this.element = dummy;
+        const e = document.createElement('textarea');
+        e.style.position = 'absolute';
+        e.style.left = '-10em';
+        e.style.top  = '-10em';
+        e.style.fontSize = '16px';
+        document.body.appendChild(e);
+        this.element = e;
     }
     show() {
         this.element.focus();
