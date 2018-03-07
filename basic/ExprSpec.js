59cce17c51acfe13bfe99936036ed6e1441092df
KENZ <KENZ.gelsoft@gmail.com>
2018-03-07 12:40:27 +0900

    More Expr tests

--- basic/ExprSpec.js
+++ basic/ExprSpec.js
@@ -2,9 +2,20 @@ describe('Expr', function () {
     function parse(aStr) {
         const ss = new StringStream(aStr);
         const tr = new Tokenizer(ss);
-        return Expr.parse(tr);
+        return Expr.parse(tr).toJS();
     }
     it('should parse a integer', function () {
-       expect(parse('1')).not.toBe(null);
+        expect(parse('1')).toBe('1');
+        expect(parse('2')).toBe('2');
+        expect(parse('10')).toBe('10');
+        expect(parse('256')).toBe('256');
+        expect(parse('01')).toBe('01');
+    });
+    it('should parse a integer with another token', function () {
+        expect(parse('1,')).toBe('1');
+        expect(parse('2=')).toBe('2');
+        expect(parse('10""')).toBe('10');
+        // FIXME following doesn't stop
+        // expect(parse('10"')).toBe('10');
     });
 });
